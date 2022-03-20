5e035c25f11a3c0a6141bb75532bc1b19bd1ae0b
kaz95 <smurfrektshrekt@gmail.com>
2022-03-20 12:48:20 -0500

    io example file

--- fc_improved.c
+++ fc_improved.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 
+#define LOWER 0 // lower limit of table
+#define UPPER 300 // upper limit
+#define STEP 20 // step size
+
 // print F->C table
 int main()
 {
 	int fahr;
 
-	for (fahr = 0; fahr <= 300; fahr = fahr + 20);
-	       //some other shits here
+	for (fahr = UPPER; fahr >= LOWER; fahr = fahr - STEP)
+		printf("%d\t%.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
+	return 0;
 }	
 
